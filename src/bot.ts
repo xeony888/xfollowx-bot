0638618f9aa6cc6cd147299e6121dff286471610
josh handelman <handelmanjos@gmail.com>
2024-09-11 19:02:08 +0100

    stuff

---
+++ src/bot.ts
@@ -0,0 +1,155 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, Client, Guild, IntentsBitField, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import dotenv from "dotenv";
+import { checkServerLink, getTwitters, makeServerLink } from "./utils";
+import axios from "axios";
+
+dotenv.config();
+
+axios.defaults.validateStatus = (status: number) => {
+    return true;
+};
+const client = new Client({ intents: [IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.Guilds] });
+const MINUTES_15 = 900000;
+client.once("ready", () => {
+    console.log(`logged in as ${client.user?.tag}`);
+    setInterval(loop, MINUTES_15);
+});
+
+async function loop() {
+    await client.guilds.fetch();
+    client.guilds.cache.forEach(async (guild) => {
+        let channel: any = guild.channels.cache.find(channel => channel.name === "xfollowx");
+        // If the channel doesn't exist, create it
+        if (!channel) {
+            channel = await guild.channels.create({
+                name: "xfollowx",
+                type: ChannelType.GuildText,
+                permissionOverwrites: [
+                    {
+                        id: guild.id, // Deny everyone
+                        deny: ["SendMessages"],
+                        allow: ["ViewChannel"]
+                    },
+                    {
+                        id: guild.client.user.id, // Allow the bot to send messages
+                        allow: ["SendMessages", "ViewChannel"]
+                    }
+                ]
+            });
+        }
+        await guild.members.fetch();
+        const memberIds = guild.members.cache.map(member => member.id);
+        const data = await getTwitters(memberIds);
+        if (!data) return;
+        for (const point of data) {
+            const rows = [];
+            let messageContent = `Discord name: ${point.discordName}\n`;
+            for (const twitter of point.twitters) {
+                const twitterButton = new ButtonBuilder()
+                    .setLabel(`Go to @${twitter}`)
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(`https://x.com/${twitter}`);
+                const row = new ActionRowBuilder().addComponents(twitterButton);
+                rows.push(row);
+            }
+            await channel.send({
+                content: messageContent,
+                components: rows
+            });
+        }
+    });
+}
+client.on("guildCreate", async (guild: Guild) => {
+    try {
+        const channel1 = await guild.channels.create({
+            name: "xfollowx",
+            type: ChannelType.GuildText,
+            permissionOverwrites: [
+                {
+                    id: guild.id,
+                    deny: ["SendMessages"],
+                    allow: ["ViewChannel"]
+                },
+                {
+                    id: guild.client.user.id,
+                    allow: ["SendMessages", "ViewChannel"]
+                }
+            ]
+        });
+        const channel2 = await guild.channels.create({
+            name: 'xfollowx-admin',
+            type: ChannelType.GuildText,
+            permissionOverwrites: [
+                {
+                    id: guild.id,
+                    allow: ["ViewChannel", "SendMessages"],
+                },
+            ],
+        });
+        const row2 = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('server_link') // this is where users go to get their roles
+                    .setLabel('Link to Server')
+                    .setStyle(ButtonStyle.Primary),
+                new ButtonBuilder()
+                    .setCustomId("check_link_status")
+                    .setLabel("Check Link Status")
+                    .setStyle(ButtonStyle.Secondary),
+                new ButtonBuilder()
+                    .setCustomId("loop")
+                    .setLabel("Reload User X Accounts")
+                    .setStyle(ButtonStyle.Danger)
+            );
+        await channel2.send({
+            content: 'Admin Page. Make sure to link your server to XFollowX.',
+            // @ts-ignore
+            components: [row2]
+        });
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+
+client.on("interactionCreate", async (interaction) => {
+    if (interaction.isButton()) {
+        if (interaction.customId === "check_link_status") {
+            const result = await checkServerLink(interaction.guild!.id);
+            if (result) {
+                await interaction.reply({ content: `Successfully linked to ${result.id}`, ephemeral: true });
+            } else {
+                await interaction.reply({ content: "Server not linked", ephemeral: true });
+            }
+        } else if (interaction.customId === 'server_link') {
+            const modal = new ModalBuilder()
+                .setCustomId('server_link_form')
+                .setTitle('Verification Form')
+                .addComponents(
+                    // @ts-ignore
+                    [new ActionRowBuilder().addComponents(
+                        new TextInputBuilder()
+                            .setCustomId('server_link_text_input')
+                            .setLabel('Enter your Lens project ID')
+                            .setStyle(TextInputStyle.Short)
+                            .setRequired(true)
+                    )]
+                );
+            await interaction.showModal(modal);
+        } else if (interaction.customId === "loop") {
+            loop();
+            await interaction.reply({ content: "...", ephemeral: true });
+        }
+    } else if (interaction.isModalSubmit() && interaction.customId === "server_link_form") {
+        const input = interaction.fields.getTextInputValue("server_link_text_input");
+        const status = await makeServerLink(interaction.guild!.id, interaction.user.id, input);
+        if (status) {
+            await interaction.reply({ content: "Link success!", ephemeral: true });
+        } else {
+            await interaction.reply({ content: `Link Failure...`, ephemeral: true });
+        }
+    }
+});
+
+
+client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ src/utils.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+export async function makeServerLink(guildId: string, userId: string, serverId: string) {
+    const response = await axios.post(`${process.env.SERVER_URL}/bot/link?key=${process.env.DISCORD_BOT_TOKEN}`, {
+        guildId,
+        userId,
+        serverId
+    });
+    if (response.status === 200) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export async function checkServerLink(guildId: string) {
+    const response = await axios.get(`${process.env.SERVER_URL}/bot/link?key=${process.env.DISCORD_BOT_TOKEN}&guildId=${guildId}`);
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        return undefined;
+    }
+}
+export async function getTwitters(memberIds: string[]) {
+    const response = await axios.get(`${process.env.SERVER_URL}/bot/twitters?key=${process.env.DISCORD_BOT_TOKEN}&memberIds=${memberIds}`);
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        return [];
+    }
+}
